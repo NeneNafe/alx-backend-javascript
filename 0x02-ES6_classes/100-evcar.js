5baade3fb185c1a6cbc0125d2636a627bad3ff02
NeneNafe <nenechiwaya@gmail.com>
2024-04-12 13:15:59 +0200

    Corrections

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -2,11 +2,11 @@ import Car from './10-car';
 
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
-    super(brand, color, motor);
+    super(brand, motor, color);
     this._range = range;
   }
 
   cloneCar() {
-    return new Car;
+    return new Car(this.brand, this.motor, this.color);
   }
 }
