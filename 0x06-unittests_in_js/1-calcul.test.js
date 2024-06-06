06312c733849cb9623cd48cb3b3fc43469e7a516
NeneNafe <nenechiwaya@gmail.com>
2024-06-06 21:33:17 +0200

    Corrections

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -17,34 +17,34 @@ describe('#calculateNumber() with type SUM', () => {
 });
 
 describe('#calculateNumber() with type SUBTRACT', () => {
-    it('sub 3 and 5', () => {
+    it('subtract 3 and 5', () => {
         assert.equal(calculateNumber('SUBTRACT', 3, 5), -2);
     });
-    it('sub 10 and 3', () => {
+    it('subtract 10 and 3', () => {
         assert.equal(calculateNumber('SUBTRACT', 10, 3), 7);
     });
-    it('sub 3.3 and 1.7', () => {
+    it('subtract 3.3 and 1.7', () => {
         assert.equal(calculateNumber('SUBTRACT', 3.3, 1.7), 1);
     });
-    it('sub -0.1 and 0.1', () => {
+    it('subtract -0.1 and 0.1', () => {
         assert.equal(calculateNumber('SUBTRACT', -0.1, 0.1), 0);
     });
 });
 
 describe('#calculateNumber() with type DIVIDE', () => {
-    it('div 15 and 5', () => {
+    it('divide 15 and 5', () => {
         assert.equal(calculateNumber('DIVIDE', 15, 5), 3);
     });
-    it('div 10 and 2', () => {
+    it('divide 10 and 2', () => {
         assert.equal(calculateNumber('DIVIDE', 10, 2), 5);
     });
-    it('div 3.3 and 3.3', () => {
+    it('divide 3.3 and 3.3', () => {
         assert.equal(calculateNumber('DIVIDE', 3.3, 3.3), 1);
     });
-    it('div 75 and 3', () => {
+    it('divide 75 and 3', () => {
         assert.equal(calculateNumber('DIVIDE', 75, 3), 25);
     });
-    it('div 1.4 and 0'), () => {
+    it('divide 1.4 and 0'), () => {
         assert.equal(calculateNumber('DIVIDE', 1.4, 0), 'Error')
     }
 });
